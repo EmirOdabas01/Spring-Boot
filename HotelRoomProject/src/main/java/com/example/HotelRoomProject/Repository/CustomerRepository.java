e3720f4aad064eeb0af80048582308f9984c8949
EmirOdabas01 <159827598+EmirOdabas01@users.noreply.github.com>
2024-02-23 23:50:47 +0300

    Delete HotelRoomProject/src directory

--- HotelRoomProject/src/main/java/com/example/HotelRoomProject/Repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.HotelRoomProject.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.HotelRoomProject.Entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-}
